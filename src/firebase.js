ab7b3396219167cac52a37a229b91e7d7247358a
Demozath Katsuragi <demozath@MacBook-Pro-de-Demozath.local>
2023-09-22 20:20:52 -0300

    primer commit desde el mac

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAsO7XKT255WS0eSL425ro8zyhJKLV9YdM",
@@ -8,7 +8,7 @@ const firebaseConfig = {
   storageBucket: "callerregister.appspot.com",
   messagingSenderId: "52009717298",
   appId: "1:52009717298:web:5ce23d764b4a1ba72d362b",
-  measurementId: "G-P43YR64Q2W"// Este es opcional si no lo necesitas
+  measurementId: "G-P43YR64Q2W"
 };
 
 const app = initializeApp(firebaseConfig);
