7d90672285333ffe0a76ffd869e7e4342c5a0c2d
Demozath Katsuragi <demozath@MacBook-Pro-de-Demozath.local>
2023-09-22 20:31:27 -0300

    Se logra conexión entre el código y la base de datos

--- src/views/registro.js
+++ src/views/registro.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
+import { collection, addDoc } from 'firebase/firestore';
 import db from '../firebase.js';
-import { collection } from 'firebase/firestore';
 
 function RegistroUsuarios() {
-    const [nombre, setNombre] = useState('');
-    const [apellido, setApellido] = useState('');
-    const [rut, setRut] = useState('');
-    const [fechaNacimiento, setFechaNacimiento] = useState('');
-    const [correo, setCorreo] = useState('');
-    const [celular, setCelular] = useState('');
-  
-    const crearUsuario = () => {
-      db.collection('usuarios').add({
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [rut, setRut] = useState('');
+  const [fechaNacimiento, setFechaNacimiento] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [celular, setCelular] = useState('');
+
+  const crearUsuario = async () => {
+    try {
+      const docRef = await addDoc(collection(db, 'usuarios'), {
         nombre: nombre,
         apellido: apellido,
         rut: rut,
@@ -19,7 +20,11 @@ function RegistroUsuarios() {
         correo: correo,
         celular: celular,
       });
+      console.log('Usuario creado con ID: ', docRef.id);
+    } catch (e) {
+      console.error('Error al añadir el documento: ', e);
     }
+  };
 
   return (
     <div>
@@ -39,15 +44,15 @@ function RegistroUsuarios() {
         </div>
         <div>
           <label>Fecha de Nacimiento:</label>
-          <input type="text" value={fechaNacimiento} onChange={(e) => setFechaNacimiento(e.target.value)} />
+          <input type="date" value={fechaNacimiento} onChange={(e) => setFechaNacimiento(e.target.value)} />
         </div>
         <div>
           <label>Correo:</label>
-          <input type="text" value={correo} onChange={(e) => setCorreo(e.target.value)} />
+          <input type="email" value={correo} onChange={(e) => setCorreo(e.target.value)} />
         </div>
         <div>
           <label>Celular:</label>
-          <input type="text" value={celular} onChange={(e) => setCelular(e.target.value)} />
+          <input type="tel" value={celular} onChange={(e) => setCelular(e.target.value)} />
         </div>
         <button type="button" onClick={crearUsuario}>Crear Usuario</button>
       </form>
